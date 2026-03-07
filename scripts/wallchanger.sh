8a68f6e3087d13dc81d4d350054aa94f05bd4498
Capital <kerian.theureaux@gmail.com>
2026-03-07 14:09:58 +0100

    startup file

---
+++ scripts/wallchanger.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dir="$HOME/Wallpapers"
+
+SELECTED=$(find "$dir" -type f \( \
+  -iname "*.jpg" -o \
+  -iname "*.png" -o \
+  -iname "*.jpeg" \
+  \) | wofi --dmenu --prompt "Choose Wallpaper")
+
+[ -z "$SELECTED" ] && exit 0
+
+swww img "$SELECTED" --transition-type wipe --transition-duration 1
